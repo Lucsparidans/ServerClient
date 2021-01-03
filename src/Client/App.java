239c5679bfec5f7725cd9ae212b88727d68b579a
Lucsparidans <lucsnadiraps@gmail.com>
2021-01-03 13:15:50 +0100

    FU Stijn

--- src/Client/App.java
+++ src/Client/App.java
@@ -8,15 +8,8 @@ public class App {
         }catch (InterruptedException e){
             e.printStackTrace();
         }
-        Client c = new Client("src/JSON_files/config_1.json");
-        Client c2 = new Client("src/JSON_files/config_2.json");
-        Client c3 = new Client("src/JSON_files/config_3.json");
-        Client c4 = new Client("src/JSON_files/config_4.json");
-        Client c5 = new Client("src/JSON_files/config_5.json");
-        new Thread(c).start();
-        new Thread(c2).start();
-        new Thread(c3).start();
-        new Thread(c4).start();
-        new Thread(c5).start();
+        for (int i = 1; i < 6; i++) {
+            new Thread(new Client(String.format("src/JSON_files/config_%d.json",i))).start();
+        }
     }
 }
